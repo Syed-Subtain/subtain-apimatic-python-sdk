687a859d93ba2d854020ee9b85a51bbe2b93fb39
Syed-Subtain <syed.subtain@apimatic.io>
2024-11-13 07:11:25 +0000

    Automated commit message

--- advancedbilling/controllers/base_controller.py
+++ advancedbilling/controllers/base_controller.py
@@ -30,7 +30,7 @@ class BaseController(object):
 
     @staticmethod
     def user_agent():
-        return 'AB SDK Python:9.1.4 on OS {os-info}'
+        return 'AB SDK Python:9.1.5 on OS {os-info}'
 
     @staticmethod
     def user_agent_parameters():
